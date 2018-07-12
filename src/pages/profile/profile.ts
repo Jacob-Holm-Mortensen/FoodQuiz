2d1daf769d389ae2a23ae69451337e31588c4cb4
jmorte14 <jmorte14@student.aau.dk>
2018-07-12 13:10:43 +0200

    Refactor profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -9,16 +9,31 @@ export class ProfilePage {
   topTen: {
     userName: string,
     score: number
-  }[];
+  }[] = [];
   
   constructor(public navCtrl: NavController) {
 
   }
 
   ionViewWillEnter(){
-    this.topTen = [{userName: 'Dick', score: 1000}, {userName: 'Jacob', score: 753}, {userName: 'Tommy', score: 751}, {userName: 'Millo', score: 14}];
-    for (let i = 0; i < 6; i++) {
+    this.setTopTen();
+  }
+
+  setTopTen(){
+    //// Get top ten
+    //
+    //// Get top ten
+    for (let i = this.topTen.length; i < 10; i++) {
       this.topTen.push({userName: '', score: -1});
     }
+    while(this.topTen.length > 10){
+      this.topTen.pop();
+    }
+    //// Test data
+    this.topTen[0] = {userName: 'Dick', score: 1000};
+    this.topTen[1] = {userName: 'Jacob', score: 753};
+    this.topTen[2] = {userName: 'Tommy', score: 751};
+    this.topTen[3] = {userName: 'Millo', score: 14};
+    //// test data
   }
 }
